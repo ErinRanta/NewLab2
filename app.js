22630229fd1f330815356f4045d82afff730cd58
Erin Ranta <erinran99@gmail.com>
2022-06-18 11:04:15 -0400

    added telling user right answer amount

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 'use strict'
+let rightAnswers = 0
 
 
 let userName = prompt("What is your name? Get to know me a bit...")
@@ -9,7 +10,9 @@ alert("Hello " + userName + " A " + "few " + "things " + "about " + "me...");
 function Question(question1, question2, question3, question4, question5){
  question1 = prompt("Hi! Was I a ballet dancer?");
     if (question1 == "yes"){
+        rightAnswers++;
         alert("You're right!")
+
     } else {(question1 == "no");  
         alert("You're wrong");
     }
@@ -18,12 +21,14 @@ function Question(question1, question2, question3, question4, question5){
  question2 = prompt("Do I have a daughter?");
 if (question2 == "yes"){
     alert("You're right!")
+    rightAnswers++;
 } else { (question2 == "no"); 
     alert("you're wrong");
 } 
 
 question3 = prompt("Do I love sports?");
 if (question3 == "yes"){
+    rightAnswers++;
     alert("Wrong")
 } else {(question3 == "no"); 
     alert("You're right!");
@@ -31,6 +36,7 @@ if (question3 == "yes"){
 
  question4 = prompt('Do I love to swim?');
 if (question4 == "yes"){
+    rightAnswers++;
     alert("You're right!")
 } else{ (question4 == "no"); 
     alert("You're wrong");
@@ -40,6 +46,7 @@ if (question4 == "yes"){
 
  question5 = prompt("Do I love coding?");
 if (question5 === "yes"){
+    rightAnswers++;
     alert("You're right")
 } else { (question5 === "no"); 
  alert("You're wrong");
@@ -62,6 +69,7 @@ while ((numberGuesses < allowedAttempts) && !correctAnswerFound) {
           if(response.toLowerCase()  === favoriteFlavors[i].toLowerCase()){
             alert('correct');
                 correctAnswerFound = true;
+                rightAnswers++;
                 break;
 
             }
@@ -76,11 +84,7 @@ let questions = [
     'How many dogs do I have?',
     'Am I a Sagittarius?'
 ];
-// let answers = [
-//     "SEATTLE",
-//     "2",
-//     "YES"
-// ]
+
 
 let answers = [
     "SEATTLE",
@@ -99,29 +103,33 @@ console.log("responses", responses);
 function checkAnswers(qNumber){
     if (responses[qNumber] === answers[qNumber] ){
         alert("you got it right");
+        rightAnswers++;
     } else alert("You're wrong")
     }
 
-    let numQuest = prompt("Guess a number between 1 and 20");
-    if (numQuest == 6) {
-alert("Yes, You are right!")
-        tally = tally + 1;
-    } else {
-        for (let i = 4; i >=0; i--){
-            if (numQuest == 6) {
-                alert("Yes, you are right!");
-                tally = tally + 1;
-                break;
-            } else if (i == 0) {
-                alert("No more guesses! THe answer is 6")
-                break;
-            } else if (numQuest < 6) {
-                numQuest = prompt("Too low, guess a number between 1 and 20. " + i + " guesses remaining.");
-            } else if (numQuest > 6) {
-                numQuest =  prompt("Too High! Guess a number between 1 and 20. " + i + " guesses remain.");
-            }
-         }  
-     }
+    function guessNumber() {
+        let correctAnswer = 6;
+        for (let i = 0; i <= 3; i++) {
+        
+          let guessedNumber = prompt('I am thinking of a number 1-20');
+          if (guessedNumber == correctAnswer) {
+            alert('You are correct!');
+            if (guessedNumber == correctAnswer) { rightAnswers++; }
+            break;
+          }
+          if (guessedNumber < correctAnswer) { alert('Your answer is too low'); }
+          if (guessedNumber > correctAnswer) { alert('Your number is too high!'); }
+          if (i == 3) { alert('done'); }
+        }
+
+        alert (name + 'you got ' + rightAnswers + ' right answers!!');
+
+      }
+      
+      guessNumber();
+        
+        
+     
     
   
     
